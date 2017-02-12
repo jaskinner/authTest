812b8ccec28a0fd7a3977dbea51d5a30d8484810
Jon <jon.a.skinner@me.com>
2017-02-12 16:41:17 -0500

    Auth0 quickstart

---
+++ app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: '50RS5gAli1rAhsF95eWKe7ZLBb9UA537',
+    domain: 'jon-ssi.auth0.com'
+};
